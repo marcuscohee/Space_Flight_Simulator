de36209764fd0fcd30adb89ca85339a6ec2357c1
Marcus Cohee <82741496+marcuscohee@users.noreply.github.com>
2022-07-28 20:36:31 -0500

    Initial commit

---
+++ Assets/Scripts/camBehavior.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camBehavior : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera _cockpitView;
+    [SerializeField] private CinemachineVirtualCamera _3rdPersonView;
+    [SerializeField] private Vector3 _mousePosition;
+    [SerializeField] private Vector3 _newMousePosition;
+    [SerializeField] private bool _hasCountdownStarted = false;
+    [SerializeField] private bool _hasCoroutineStarted = false;
+    [SerializeField] private GameObject _cinematicScene;
+
+    private void Start()
+    {
+        _cockpitView.Priority = 15;
+        _3rdPersonView.Priority = 10;
+        _mousePosition = Input.mousePosition;
+        _newMousePosition = Input.mousePosition;
+    }
+
+    void Update()
+    {
+        SwitchingCameras();
+        CheckingInput();
+    }
+
+    void SwitchingCameras()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && _cockpitView.Priority > 10)
+        {
+            _3rdPersonView.Priority = 15;
+            _cockpitView.Priority = 10;
+        }
+        else if (Input.GetKeyDown(KeyCode.R) && _3rdPersonView.Priority > 10)
+        {
+            _cockpitView.Priority = 15;
+            _3rdPersonView.Priority = 10;
+        }
+    }
+
+    void CheckingInput()
+    {
+        _mousePosition = Input.mousePosition;
+       if (_mousePosition != _newMousePosition || Input.anyKey)
+       {
+            Debug.Log("Input Detected");
+            _hasCountdownStarted = false;
+            _cinematicScene.SetActive(false);
+            _newMousePosition = Input.mousePosition;
+            _hasCoroutineStarted = false;
+            StopAllCoroutines();
+       }
+       else
+       {
+            Debug.Log("Countdown Has Started");
+            _hasCountdownStarted = true;
+            if (_hasCoroutineStarted == true)
+            {
+                return;
+            }
+            StartCoroutine(StartCinematicSequence());
+       }
+    }
+
+    IEnumerator StartCinematicSequence()
+    {
+        _hasCoroutineStarted = true;
+        yield return new WaitForSeconds(5.0f);
+        Debug.Log("In Coroutine");
+        _cinematicScene.SetActive(true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Space_Flight_Simulator
+ This is a space flight simulator with elements of both Timeline and Cinemachine in Unity!
